3b4e3225e160829e5ecf66f425d4c36cbb86e128
T-Bone-Willson <32392016+T-Bone-Willson@users.noreply.github.com>
2019-02-25 14:18:05 +0000

    Root Choices - Framework Started
    Instead of just running the "C" choices while loop at start of program, it will now ask their intial choice "a, b or c". At this point when chosen "c" it will take the user through the C branch choices which is essentially a linear While Loop.
    
    This starts the framework for the user to intiate the other branches such as "A" and "B"

--- Prototype1/Prototype1.py
+++ Prototype1/Prototype1.py
@@ -10,75 +10,36 @@
 #Empty List to contain user input
 UserChoice = []
 
-ops = True
-###
-##
-##
-##
 #Goes through C options
-while ops:
-    choice = input("You can choose 'c': ")
-    if choice == "c":
-        UserChoice.append(choice)
-        print(UserChoice)
-        if UserChoice[-1] == "c":
-            choice = input("You can choose 'c1': ")
+def Cdecision(RootChoice):
+    ops = True #variable was global, but made it local due to "UnboundLocalError"
+    while ops:
+        choice = input("You can choose 'c': ")
+        if choice == "c":
             UserChoice.append(choice)
             print(UserChoice)
-            if UserChoice[-1] == "c1":
-                choice = input("You can choose 'c2': ")
+            if UserChoice[-1] == "c":
+                choice = input("You can choose 'c1': ")
                 UserChoice.append(choice)
                 print(UserChoice)
-                if UserChoice[-1] == "c2":
-                    choice = input("You can choose 'c3': ")
+                if UserChoice[-1] == "c1":
+                    choice = input("You can choose 'c2': ")
                     UserChoice.append(choice)
                     print(UserChoice)
-                    if UserChoice[-1] == "c3":
-                        #print(UserChoice)
-                        ops = False
-                        #break
+                    if UserChoice[-1] == "c2":
+                        choice = input("You can choose 'c3': ")
+                        UserChoice.append(choice)
                         print(UserChoice)
-    else:
-        ops = False
-        print("Sorry You're not a winner")
+                        if UserChoice[-1] == "c3":
+                            #print(UserChoice)
+                            ops = False
+                            #break
+                            print(UserChoice)
+        else:
+            ops = False
+            print("Sorry You're not a winner")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###############################################################
-#input variable
-#dec = input("Please choose: ")
-
-#Choices if-elif-else chain
-#if dec == "c":
-    #UserChoice.extend("c")
-    #input("Your following options are: c1 ")
-    #if dec == "c1":
-        #UserChoice.extend("c1")
-        #print(UserChoice)
-        #input("Your following options are: c2 ")
-        #if dec == "c2":
-            #UserChoice.extend("c2")
-            #print(UserChoice)
-    #print(UserChoice)
-
-
-#checking type of name
-#print(type(dec))
-
-#print("Test")
-
-#pseudo code
-#if choices == 'a':
+#Presents user with Root choices of a, b, or c. Dependent on choice, will call it's associated Function
+RootChoice = input("Choose a, b or c: ")
+if RootChoice == "c":
+    Cdecision(RootChoice)
