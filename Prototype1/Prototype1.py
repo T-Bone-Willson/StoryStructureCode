e5a177b51d53775e3fe5a25d52a63fd3702df31c
T-Bone-Willson <32392016+T-Bone-Willson@users.noreply.github.com>
2019-02-25 15:07:01 +0000

    Root Choice Framwork - B branch
    B branch development begun. User can now call the "Bdecision" function when Root condition has been met.

--- Prototype1/Prototype1.py
+++ Prototype1/Prototype1.py
@@ -3,8 +3,8 @@ UserChoice = []
 
 #Goes through C Branch options
 def Cdecision(RootChoice):
-    ops = True #variable was global, but made it local due to "UnboundLocalError"
-    while ops:
+    opsC = True #variable was global, but made it local due to "UnboundLocalError"
+    while opsC:
         choice = input("You can choose 'c': ")
         if choice == "c":
             UserChoice.append(choice)
@@ -30,12 +30,25 @@ def Cdecision(RootChoice):
             ops = False
             print("Sorry You're not a winner")
 
+#Goes through B Branch options
+def Bdecision(RootChoice):
+    opsB = True
+    while opsB:
+        choice = input("You can choose 'b': ")
+        if choice == "b":
+            UserChoice.append(choice)
+            print(UserChoice)
+        else:
+            opsB = False
+            print("This is all there is right now")
+
+
 #Presents user with Root choices of a, b, or c. Dependent on choice, will call it's associated Function
 RootChoice = input("Choose a, b or c: ")
 if RootChoice == "a":
     print("A Branch is not made yet!")
 elif RootChoice == "b":
-    print("B Branch is not made yet!")
+    Bdecision(RootChoice)
 elif RootChoice == "c":
         Cdecision(RootChoice)
 else:
