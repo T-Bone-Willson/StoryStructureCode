9eef89b7768d8bc354a0c9b79ba72beb7fa342a5
T-Bone-Willson <32392016+T-Bone-Willson@users.noreply.github.com>
2019-02-25 14:53:47 +0000

    Root Choice Framework
    Two reqs been done:
    1. Inform User A branch and B Branch are not made yet when condition is met
    2. Inform user when chosen uncompatible choice from RootChoice
    
    ############################
    Have removed clutterd comments too

--- Prototype1/Prototype1.py
+++ Prototype1/Prototype1.py
@@ -1,16 +1,7 @@
-
-
-
-#Empty List for choices that user makes. Appended onto here
-#UserChoice = []
-#List of choices that can be made
-#choices = ['a', 'a1', 'a2', 'a3', 'a4', 'a5', 'b', 'b1', 'b2', 'b3', 'b4', 'ab1', 'ab2', 'c', 'c1', 'c2', 'c3']
-#################################################################
-
 #Empty List to contain user input
 UserChoice = []
 
-#Goes through C options
+#Goes through C Branch options
 def Cdecision(RootChoice):
     ops = True #variable was global, but made it local due to "UnboundLocalError"
     while ops:
@@ -41,7 +32,11 @@ def Cdecision(RootChoice):
 
 #Presents user with Root choices of a, b, or c. Dependent on choice, will call it's associated Function
 RootChoice = input("Choose a, b or c: ")
-if RootChoice == "c":
-    Cdecision(RootChoice)
-
-    # TEST WORKED - Can Now commit to Prototype-1 branch without Atom causing Run/Compiling errors.
+if RootChoice == "a":
+    print("A Branch is not made yet!")
+elif RootChoice == "b":
+    print("B Branch is not made yet!")
+elif RootChoice == "c":
+        Cdecision(RootChoice)
+else:
+    print("SORRY! Invalid choice!")
