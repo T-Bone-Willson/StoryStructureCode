b628eb516667429a01ea8d38b0862144f38e2ceb
T-Bone-Willson <32392016+T-Bone-Willson@users.noreply.github.com>
2019-03-14 13:21:35 +0000

    C_Chain + Test
    Will start making the C_Chain which allows users to choose which nodes they want from the C Branch. Did a quick test to see if "c" would be added to UserChoice List, once C_Chain had been chosen.

--- Prototype2/Prototype2.py
+++ Prototype2/Prototype2.py
@@ -47,3 +47,47 @@ g = graph(graph_elements)
 #Print test statement
 print(g.getNodes()) # Print all nodes in graph
 print(g.getEdges()) # print all edges within graph
+
+#def Cdecision(self, RootChoice = 'c'):
+#    opsC = True
+#    while opsC:
+#        choice = input("You can choose 'c': ")
+#        if choice == "c":
+#            UserChoice.append(choice)
+#            self.graph_dictionary.key()
+#            if self.graph_dictionary.key() == "c":
+#                choice = input("You can choose 'c1': ")
+#                UserChoice.append(choice)
+#                self.graph_dictionary.key()
+#                if self.graph_dictionary.key() == "c1":
+#                    choice = input("You can choose 'c2': ")
+#                    UserChoice.append(choice)
+#                    self.graph_dictionary.key()
+#                    if self.graph_dictionary.key() == "c2":
+#                        choice = input("You can choose 'c3': ")
+#                        UserChoice.append(choice)
+#                        self.graph_dictionary.key()
+#                        print(UserChoice)
+#                        print(g.Cdecision())
+#        else:
+#            print("Wrong Input!")
+
+def C_Chain(RootChoice):
+    UserChoice.append("c")
+    print(UserChoice)
+
+
+
+
+
+
+#Presents user with Root choices of a, b, or c. Dependent on choice, will call it's associated Function
+RootChoice = input("Choose a, b or c: ")
+#if RootChoice == "a":
+    #Adecision(RootChoice)
+#elif RootChoice == "b":
+    #Bdecision(RootChoice)
+if RootChoice == "c":
+    C_Chain(RootChoice)
+else:
+    print("SORRY! Invalid choice!")
