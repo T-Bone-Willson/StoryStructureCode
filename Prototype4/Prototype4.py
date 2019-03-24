7fc0e815cafe312d118913970dc2cda37fb00d76
T-Bone-Willson <32392016+T-Bone-Willson@users.noreply.github.com>
2019-03-24 16:27:51 +0000

    Prototype 4 - Finished
    Created Prototype4 just to test out an idea I had, and I didn't want to mess/alter with the codebase in Prototype 3 untill I knew it would work.
    
    Look at Prototype 4 development report within disertation to understand more.

--- Prototype4/Prototype4.py
+++ Prototype4/Prototype4.py
@@ -1,1 +1,44 @@
-print("Hello World")
+# users choice stored in an empty list.
+UserChoice = []
+
+TemplateList = []
+
+def A_Chain(RootChoice):
+    # Node a is "Home: Protagonist is there and they want the Elixir"
+    UserChoice.append("a") # Adds Branch root to List
+    a = "a: Protagonist starts at Home\n"
+    TemplateList.append(a)
+
+    # Node a1 is "Elixir: It is inanimate object"
+    # Node a2 is "Elixir: Is another charecter"
+    choice = input("You can choose 'a1' or 'a2': ")
+    if choice == "a1": # player needs to input this in order to proceed through branch
+        UserChoice.append(choice)
+        #a1 == "a1: The Elixir is an object"
+        TemplateList.append(str("Elixir: It is inanimate object\n"))
+        print(UserChoice)
+        a1()
+    elif choice == "a2": # Will unlock a2() function/branch.
+        UserChoice.append(choice)
+        #a2 == "a2: The Elixir is a person"
+        TemplateList.append(str("The Elixir is a person\n"))
+        print(UserChoice)
+        a2()
+    else:
+        print("Test a1 and a2: Invalid choice!")
+
+def a1():
+    print("")
+    for elem in TemplateList:
+        print(elem)
+
+def a2():
+    print("")
+    for elem in TemplateList:
+        print(elem)
+
+RootChoice = input("Choose a: ")
+if RootChoice == "a":
+    A_Chain(RootChoice) # Goes to Adventure Branch Code
+else:
+    print("You failed")
